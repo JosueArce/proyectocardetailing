734889b4686e91afeadcef39292ac296fcf3567b
Josué Arce <josue.arce.gonzalez@gmail.com>
2026-09-10 20:26:16 -0600

    Fix header logo test selector

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: { ecmaVersion: 2020, globals: { ...globals.browser, ...globals.node }, parserOptions: { ecmaVersion: 'latest', ecmaFeatures: { jsx: true }, sourceType: 'module' } },
+    plugins: { 'react-hooks': reactHooks, 'react-refresh': reactRefresh },
+    rules: { ...js.configs.recommended.rules, ...reactHooks.configs.recommended.rules, 'react-refresh/only-export-components': ['warn', { allowConstantExport: true }] },
+  },
+  { files: ['src/**/*.test.{js,jsx}', 'src/test/**/*.{js,jsx}'], languageOptions: { globals: globals.vitest } },
+]
